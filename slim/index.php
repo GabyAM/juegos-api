43c3728b3bcb6a92ed2f19b8e42fd824895527dc
GabyAM <gdgabyam@gmail.com>
2024-09-28 23:21:10 -0300

    Adds custom error handler

--- slim/index.php
+++ slim/index.php
@@ -1,15 +1,37 @@
 <?php
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
+class CustomException extends Exception
+{
+}
 
 $app = AppFactory::create();
 
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
+$customErrorHandler = function (Request $request, Throwable $exception) use ($app) {
+    $exceptionCode = $exception->getCode();
+    if ($exception instanceof CustomException) {
+        $responseCode = $exceptionCode;
+    } else {
+        $responseCode = 500;
+    }
+
+    $errorMessage = $responseCode === 500 ? "Error de servidor interno" : $exception->getMessage();
+    $payload = ['status' => $responseCode, 'error' => $errorMessage];
+    $response = $app->getResponseFactory()->createResponse();
+    $response->getBody()->write(json_encode($payload, JSON_UNESCAPED_UNICODE));
+
+    return $response->withStatus($responseCode);
+};
+
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
-$app->add( function ($request, $handler) {
+$errorMiddleware->setDefaultErrorHandler($customErrorHandler);
+$app->add(function ($request, $handler) {
     $response = $handler->handle($request);
 
     return $response
